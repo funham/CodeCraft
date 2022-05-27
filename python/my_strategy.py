c8d2028c3b7391972206ab59896b8c59507547cd
xsinus <fun1ham@gmail.com>
2022-05-27 09:48:57 +0300

    deleted random '+' at the end of my_staratedy

--- python/my_strategy.py
+++ python/my_strategy.py
@@ -19,8 +19,9 @@ class MyStrategy:
     def first_time_preparation(self, game):
         global resource_planets, my_workers, planets_workers_count, builds_done, first_time_flag, types_of_buildings, taken_stone
         for planet in resource_planets:
-            builds_done[planet.id] = False #заполняем посроения false'ами
-            planets_workers_count[planet.id] = 0 # обнуляем количесвто рабочих в начале игры (чтобы не отправляли нескоько раз рабочих)
+            builds_done[planet.id] = False  # заполняем посроения false'ами
+            # обнуляем количесвто рабочих в начале игры (чтобы не отправляли нескоько раз рабочих)
+            planets_workers_count[planet.id] = 0
 
     '''
     Примитивная стратегия, которая пытается застроить всю карту каменоломнями и получать очки за добычу камня.
@@ -35,7 +36,8 @@ class MyStrategy:
         taken_stone[0] = 0
 
         for planet in game.planets:
-            if planet.harvestable_resource is not None and planet.harvestable_resource is not Resource.STONE:  # закидываем ресурс (НЕ камень)
+            # закидываем ресурс (НЕ камень)
+            if planet.harvestable_resource is not None and planet.harvestable_resource is not Resource.STONE:
                 resource_planets.append(planet)
                 taken_stone[planet.id] = 0
 
@@ -55,13 +57,15 @@ class MyStrategy:
                 # if planets_workers_count[planet.id] < 50 and planet.resources[Resource.STONE] >= min(worker[1],50):
                 #     moves.append(MoveAction(worker[0], planet.id, min(worker[1],50), Resource.STONE))
 
-                if planets_workers_count[planet.id] < 50 and Resource.STONE in game.planets[worker[0]].resources.keys(): # есть ли камни там, есть ли рабочие (< 50)
+                # есть ли камни там, есть ли рабочие (< 50)
+                if planets_workers_count[planet.id] < 50 and Resource.STONE in game.planets[worker[0]].resources.keys():
 
                     if game.planets[worker[0]].resources[Resource.STONE] - taken_stone[worker[0]] >= 50 \
                             and worker[0] == 0:
-                        taken_stone[worker[0]] += 50 # откуда отправляем
+                        taken_stone[worker[0]] += 50  # откуда отправляем
                         planets_workers_count[planet.id] = 50
-                        moves.append(MoveAction(worker[0], planet.id, 50, Resource.STONE))
+                        moves.append(MoveAction(
+                            worker[0], planet.id, 50, Resource.STONE))
                         # worker[0] - откуда, planet.id = куда
 
         for planet in resource_planets:
@@ -69,15 +73,16 @@ class MyStrategy:
                 builds_done[planet.id] = True
 
         for planet in resource_planets:
-            have_workers = False # есть ли нужное количество рабочи там
+            have_workers = False  # есть ли нужное количество рабочи там
             for worker in planet.worker_groups:
                 if worker.player_index == game.my_index and worker.number >= 50:
-                    have_workers = True # есть рабочие, которых можно эксплуатировать
+                    have_workers = True  # есть рабочие, которых можно эксплуатировать
 
             if have_workers and Resource.STONE in planet.resources.keys() and planet.resources[
-                Resource.STONE] >= 50 and not builds_done[planet.id]:
+                    Resource.STONE] >= 50 and not builds_done[planet.id]:
 
-                builds.append(BuildingAction(planet.id, types_of_buildings[planet.harvestable_resource])) # строим здание
+                builds.append(BuildingAction(
+                    planet.id, types_of_buildings[planet.harvestable_resource]))  # строим здание
 
         return (moves, builds)
 
@@ -91,4 +96,3 @@ class MyStrategy:
 
         moves, builds = self.action_generation(game)
         return Action(moves, builds, None)
-+
