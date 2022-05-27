5a61750f5e7c440e221c1fd6d41308734097ac33
Aiky_Moon <tuzat369@gmail.com>
2022-05-27 09:45:47 +0300

    strategy (find nearest)

--- python/my_strategy.py
+++ python/my_strategy.py
@@ -1,6 +1,73 @@
 from model import *
 
+resource_palnets = []
+my_workers = []
+
+def manhattan(x1, y1, x2, y2):
+    return abs(x1 - x2) + abs(y1 - y2)
+
+main_resourses_planets = {
+    Resource.ORE: None,
+    Resource.SAND: None,
+    Resource.ORGANICS: None,
+                        }
+
+
+
+needed_res = [Resource.ORE, Resource.SAND, Resource.ORGANICS]
 
 class MyStrategy:
+
+
+    def __init__(self):
+        self.main_planet = None
+        self.flag = False
+
+
+
+    def print_planet(self, game: Game):
+        resource_palnets.clear()
+        for planet in game.planets:
+            if planet.harvestable_resource is not None:
+                resource_palnets.append(planet)
+
+
+
+    def find_nearest_planets(self, game: Game):
+        if self.main_planet:
+            nearests = [float("inf"), float("inf"), float("inf")]
+
+            for planet in resource_palnets:
+                if planet.harvestable_resource in needed_res:
+                    index = needed_res.index(planet.harvestable_resource)
+                    if nearests[index] > manhattan(planet.x, planet.y, self.main_planet.x, self.main_planet.y):
+                        nearests[index] = manhattan(planet.x, planet.y, self.main_planet.x, self.main_planet.y)
+                        main_resourses_planets[planet.harvestable_resource] = planet.id
+
+
+
+
+
     def get_action(self, game: Game) -> Action:
-        return Action([], [], None)
+
+
+        moves = []
+        builds = []
+
+        self.print_planet(game)
+        if resource_palnets:
+            self.main_planet = resource_palnets[0]
+
+        self.find_nearest_planets(game)
+
+
+        print(main_resourses_planets)
+        # if not self.flag:
+        #     if self.main_planet.resources.stone > 900:
+        #         for needed_planet in main_resourses_planets.values():
+        #             moves.append(MoveAction(self.main_planet.id, needed_planet, 300, Resource.STONE))
+        #
+        #     flag = True
+
+
+        return Action(moves, [], None)
--- python/test.py
+++ python/test.py
@@ -9,40 +9,17 @@ class MyStrategy:
     def __init__(self):
         pass
 
-    def init_values(self, game):
+
+    def print_planet(self, game: Game):
         resource_palnets.clear()
-        my_workers.clear()
-
         for planet in game.planets:
-            if planet.harvestable_resource is not None:
-                resource_palnets.append(planet)
-
-            for worker_group in planet.worker_groups:
-                if worker_group.player_index == game.my_index:
-                    my_workers.append((planet.id, worker_group.number))
-
-
-    def action_generation(self, game):
-        moves = []
-        builds = []
-
-        planets_workers_count = {}
-
-        for worker in my_workers:
-            for planet in resource_palnets:
-                if planets_workers_count[planet.id] < 50 and planet.resources >= min(worker[1], 50):
-                    moves.append(MoveAction(worker[0], planet.id, min(worker[1], 50), Resource.STONE))
-
-        return (moves, builds)
-
+            if not planet.harvestable_resource is None:
+                resource_palnets.append(planet.id)
+                print(planet.id)
 
 
     def get_action(self, game: Game) -> Action:
 
-        self.init_values(game)
-
-        moves = []
-        builds = []
-
+        self.print_planet(game)
 
         return Action([], [], None)
