c1d67dfd1260332808dae4104b6fe5756855ce7d
xsinus <fun1ham@gmail.com>
2022-05-27 12:20:47 +0300

    added finding variants of nearests planets

--- python/my_strategy.py
+++ python/my_strategy.py
@@ -19,15 +19,25 @@ class MyStrategy:
         self.main_planet = None
         self.flag = False
 
-    def find_nearest_planets(self, game: Game, current_planet: Planet) -> dict:
-        res_on_planets = dict.fromkeys(MyStrategy.resources, [])
+    def find_main_planet(self, game: Game) -> Planet:
+        pass
+
+    def find_nearest_planets(self, game: Game, current_planet: Planet,
+                             filtered_resources: list = [Resource.STONE]) -> list:
+        nearests = []
 
         for p in game.planets:
-            if p.harvestable_resource != Resource.STONE:
-                res_on_planets[p.harvestable_resource].append(p)
+            if p.harvestable_resource not in filtered_resources:
+                nearests.append(p)
 
-        for _, planet in res_on_planets:
-            planet.sort(key=lambda p: dist(p, current_planet))
+        nearests.sort(key=lambda p: dist(p, current_planet))
+        return nearests
+
+    def find_nearest_planets_by_res(self, game: Game, current_planet: Planet) -> dict:
+        res_on_planets = dict.fromkeys(MyStrategy.resources, [])
+
+        for p in self.find_nearest_planets(game, current_planet):
+            res_on_planets[p.harvestable_resource].append(p)
 
         return res_on_planets
 
