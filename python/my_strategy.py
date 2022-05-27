541e95837f8b22c6346b8033efc7e25fa48fa756
xsinus <fun1ham@gmail.com>
2022-05-27 12:08:26 +0300

    separated some code from class

--- python/my_strategy.py
+++ python/my_strategy.py
@@ -1,24 +1,25 @@
 from model import *
 
-resource_palnets = []
-my_workers = []
-
 main_resourses_planets = {
     Resource.ORE: None,
     Resource.SAND: None,
     Resource.ORGANICS: None,
 }
 
+resources = (Resource.ORE, Resource.SAND, Resource.ORGANICS)
+
+
+def dist(p1: Planet, p2: Planet) -> int:
+    return abs(p1.x - p2.x) + abs(p1.y - p2.y)
+
 
 class MyStrategy:
-    resources = (Resource.ORE, Resource.SAND, Resource.ORGANICS)
 
     def __init__(self):
         self.main_planet = None
         self.flag = False
 
-    @property
-    def nearest_planets(self, game: Game, current_planet: Planet) -> dict:
+    def find_nearest_planets(self, game: Game, current_planet: Planet) -> dict:
         res_on_planets = dict.fromkeys(MyStrategy.resources, [])
 
         for p in game.planets:
@@ -26,7 +27,7 @@ class MyStrategy:
                 res_on_planets[p.harvestable_resource].append(p)
 
         for _, planet in res_on_planets:
-            planet.sort(key=lambda p: MyStrategy.dist(p, current_planet))
+            planet.sort(key=lambda p: dist(p, current_planet))
 
         return res_on_planets
 
@@ -34,12 +35,4 @@ class MyStrategy:
         moves = []
         builds = []
 
-        if resource_palnets:
-            self.main_planet = resource_palnets[0]
-
-        self.find_nearest_planets(game)
-
         return Action(moves, builds, None)
-
-    def dist(p1: Planet, p2: Planet) -> int:
-        return abs(p1.x - p2.x) + abs(p1.y - p2.y)
